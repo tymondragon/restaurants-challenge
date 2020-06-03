b3a5c8d38cdcc33db1c2c14b0566381f8649b178
Ty Mondragon <tymondragon@gmail.com>
2020-06-03 11:12:39 -0600

    successful api call

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ https://docs.google.com/document/d/1Jkzyft4zMu-v1UVoHiJM1SxO-XwSp5irH-rNi52N3i4/
 
 ## To Do's
 https://docs.google.com/document/d/1vWqtwIuyvxeU3gl9pamxX88KlQbZB1p7OyEQh39C63w/edit?usp=sharing
+
+## Google Places API
+Details of a restaurant:
+```
+https://developers.google.com/places/web-service/details
+```
+
+Find a location's place_id:
+```
+https://developers.google.com/places/place-id
+
+```
--- restaurants/restaurants.controller.js
+++ restaurants/restaurants.controller.js
@@ -1,7 +1,33 @@
 const db = require('../server/knex');
+const API_URL = process.env.GOOGLE_API_URL;
+const API_KEY = process.env.GOOGLE_API_KEY;
+const fetch = require('node-fetch');
+
+const restaurantFields = {
+  name: 'name',
+  rating: 'rating',
+  hoursOfOperation: 'opening_hours/weekday_text',
+  openNow: 'opening_hours/open_now',
+  address: 'formatted_address',
+  phoneNumber: 'formatted_phone_number',
+  website: 'website'
+}
+
+const fields = Object.values(restaurantFields);
 
 exports.list = async (req, res, next) => {
   const restaurants = await db('restaurants').select('*');
+
+  const { name, rating, hoursOfOperation, openNow, ...rest } = restaurantFields;
+  const listFields = [name, rating, hoursOfOperation, openNow].join(",");
+
+  for (const restaurant of restaurants) {
+    const json = await fetchApi(listFields, restaurant.place_id)
+    await console.log(json.result)
+  }
+
+  // const jsonResult = await fetchApi(listFields, restaurants[0].place_id)
+  // await console.log(jsonResult, "result api")
 
   res.json({
     restaurants: restaurants.map(restaurant => {
@@ -15,8 +41,17 @@ exports.list = async (req, res, next) => {
 }
 
 exports.getRestaurantById = async (req, res, next) => {
-  console.log("made it here")
-    const [restaurant] = await db('restaurants').select('name').where('id', req.params.restaurantId);
-    await console.log(restaurant)
-    res.json({ ...restaurant })
+  const [restaurant] = await db('restaurants').select('name').where('id', req.params.restaurantId);
+  // await console.log(restaurant)
+  res.json({ ...restaurant })
+}
+
+async function fetchApi (fields, place_id) {
+  const url = urlBuilder(fields, place_id);
+  const response = await fetch(url);
+  return await response.json();
+}
+
+function urlBuilder (fields, place_id) {
+  return `${API_URL}?place_id=${place_id}&fields=${fields}&key=${API_KEY}`
 }
