82c252d3441b39b0037c38d306da8daa4e68b6ad
Ty Mondragon <tymondragon@gmail.com>
2020-06-04 00:51:09 -0600

    detailed drawer displays information

--- client/src/Restaurants.js
+++ client/src/Restaurants.js
@@ -4,31 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import Restaurant from './Restaurant';
 
 const useStyles = makeStyles({
-  // root: {
-  //   minWidth: 275,
-  // },
-  // bullet: {
-  //   display: 'inline-block',
-  //   margin: '0 2px',
-  //   transform: 'scale(0.8)',
-  // },
-  // title: {
-  //   fontSize: 14,
-  // },
-  // pos: {
-  //   marginBottom: 12,
-  // },
   card: {
     marginBottom: 10
   }
 });
 
-function Restaurants ({ restaurants }) {
+function Restaurants ({ restaurants, fetchRestaurant }) {
   const classes = useStyles();
   return (
     <ul>
       {restaurants.map((restaurant => {
-        return <Restaurant className={classes.card} key={restaurant.id} restaurant={restaurant} />
+        return <Restaurant className={classes.card} key={restaurant.id} restaurant={restaurant} fetchRestaurant={fetchRestaurant}/>
       }))}
     </ul>
   );
